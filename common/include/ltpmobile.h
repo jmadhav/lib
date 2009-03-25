490b842fd1e6a9e755ed6ae1d3b7b79295250c87
mukesh <mukesh.s@geodesic.com>
2009-03-25 14:47:18 +0530

    resolve rishi  branch conflict with mukesh

--- common/include/ltpmobile.h
+++ common/include/ltpmobile.h
@@ -155,11 +155,9 @@ struct ltp
 	/* each request within a session has a different message id */
 	unsigned int32		msgid;
 	
-<<<<<<< HEAD:common/include/ltpmobile.h
-	/* a RESPONSE_AUTHENTICATE w	ill carry a challange and a subsequent request will carry an MD5 checksum (read Sec 6 of the draft) */
-=======
+
 	/* a RESPONSE_AUTHENTICATE will carry a challange and a subsequent request will carry an MD5 checksum (read Sec 6 of the draft) */
->>>>>>> mukesh:common/include/ltpmobile.h
+
 	char	authenticate[16]; 
 	
 	/* When a received request has these fields set to non-zero, you should
@@ -692,11 +690,9 @@ void ltpSoundInput(struct ltpStack *ps, short *pcm, int nsamples, int isSpeaking
 int ltpUpdateContact(struct ltpStack *ps, char *userid, char *title, char *group, unsigned short16 presence, 
 		unsigned int32 ip, unsigned short16 port, unsigned int32 fwdip, unsigned short16 fwdport,
 		char *device, char *label);
-<<<<<<< HEAD:common/include/ltpmobile.h
 
-=======
 void ltpMessageDTMF(struct ltpStack *ps, int lineid, char *msg);
->>>>>>> mukesh:common/include/ltpmobile.h
+
 /*
 void ltpRemoveAllContacts(struct ltpStack *ps);
 struct Contact *getContact(struct ltpStack *ps, char *userid);
@@ -722,8 +718,6 @@ void MD5Final(unsigned char *digest, struct MD5Context *ctx);
 }
 #endif
  
-<<<<<<< HEAD:common/include/ltpmobile.h
+
 #endif
-=======
-#endif
->>>>>>> mukesh:common/include/ltpmobile.h
+
